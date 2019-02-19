c4047716afb0125c3bab2b8fd26f0a256e10c764
mohammad mohammadi <saberzx313@gmail.com>
2019-02-19 11:13:55 +0330

    add axios to react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
-
+import { Table, Button } from 'reactstrap';
 class App extends Component {
+  state={
+    books : []
+  }
+  componentWillMount(){
+    axios("http://localhost:3000/books").then((response)=>{
+      this.setState({
+        books : response.data
+      })
+    })
+  }
   render() {
     return (
       <div className="App container">
- 
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Ratin</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Book Title</td>
+              <td>3.5</td>
+              <td>
+                <Button color="success" size="sm" className="mr-2">Edit</Button>
+                <Button color="danger" size="sm">Delete</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     );
   }
